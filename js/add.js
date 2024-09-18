23cbbdc4af6e74e705b5f1044bb0623d119b14a2
RaiyanJiyon <raiyanjiyon814@gmail.com>
2024-09-19 00:00:14 +0600

    Added add money functionality in home page

---
+++ js/add.js
@@ -0,0 +1,18 @@
+document.getElementById('add-money-button')
+    .addEventListener('click', (event) => {
+        event.preventDefault();
+        const addMoneyPin = document.getElementById('add-money-pin-number').value;
+        
+        if (addMoneyPin === '1234') {
+            let totalAmountElement = document.getElementById('total-amount');
+            let totalAmountNumber = Number(totalAmountElement.textContent);
+            console.log(totalAmountElement);
+            const addMoney = document.getElementById('add-amount-money').value;
+            const addMoneyNumber = Number(addMoney);
+            console.log(addMoney);
+            totalAmountNumber = totalAmountNumber + addMoneyNumber;
+            totalAmountElement.textContent = totalAmountNumber;
+        } else {
+            alert('Invalid Pin Number!');
+        };
+    });
