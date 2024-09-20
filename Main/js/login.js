706ceeb4568ce713a506208eb225b9e1b9eb8c30
RaiyanJiyon <raiyanjiyon814@gmail.com>
2024-09-20 22:55:16 +0600

    Added login.js file for index.html and Create home.html file

---
+++ Main/js/login.js
@@ -0,0 +1,13 @@
+document.getElementById('login-btn')
+    .addEventListener('click', (event) => {
+        event.preventDefault();
+
+        const inputPhoneNumber = document.getElementById('input-phone-number').value;
+        const inputPinCode = document.getElementById('input-pin-code').value;
+
+        if (inputPhoneNumber === '019' && inputPinCode === '1') {
+            window.location.href = "home.html";
+        } else {
+            alert('Invalid Phone Number or Pin Code');
+        }
+    })
