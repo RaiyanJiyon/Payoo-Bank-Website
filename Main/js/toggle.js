ac2d36953d782abe18683d3d68493e510f2109aa
RaiyanJiyon <raiyanjiyon814@gmail.com>
2024-09-22 11:23:24 +0600

    Added toggle functionalites in toggle.js file

---
+++ Main/js/toggle.js
@@ -0,0 +1,24 @@
+document.getElementById('add-button')
+    .addEventListener('click', () => {
+        toggleForm('add-money-form')
+    });
+
+document.getElementById('withdraw-button')
+    .addEventListener('click', () => {
+        toggleForm('withdraw-money-form')
+    });
+
+document.getElementById('transfer-button')
+    .addEventListener('click', () => {
+        toggleForm('transfer-money-form')
+    });
+
+document.getElementById('get-bonus-button')
+    .addEventListener('click', () => {
+        toggleForm('bonus-money-form')
+    });
+
+document.getElementById('pay-bill-button')
+    .addEventListener('click', () => {
+        toggleForm('pay-bill-form')
+    });
--- Main/js/utilities.js
+++ Main/js/utilities.js
@@ -9,3 +9,14 @@ function getTextElementValueAsNumber(id) {
     const textValueAsNumber = Number(textElementValue);
     return textValueAsNumber;
 }
+
+
+function toggleForm(id) {
+    document.getElementById('add-money-form').classList.add('hidden');
+    document.getElementById('withdraw-money-form').classList.add('hidden');
+    document.getElementById('transfer-money-form').classList.add('hidden');
+    document.getElementById('bonus-money-form').classList.add('hidden');
+    document.getElementById('pay-bill-form').classList.add('hidden');
+    
+    document.getElementById(id).classList.remove('hidden')
+}
